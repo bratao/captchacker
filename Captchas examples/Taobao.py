17aa5bce2f9619787340e65602bc928509cc4579
remi.paucher@student.ecp.fr <remi.paucher@student.ecp.fr@2e5afe5e-f900-11dd-b556-9305e745af9e>
2009-02-18 16:44:55 +0000

    Nouvelle organisation

--- Captchas examples/Taobao.py
+++ Captchas examples/Taobao.py
@@ -4,7 +4,16 @@ import os
 import cookielib
 import time
 
-LIEN_IMAGES = "https://login.live.com/pp600/hip.srf?int=login"
+#TRACEBACK
+import traceback
+import sys
+def Myexcepthook(type, value, tb):
+        lines=traceback.format_exception(type, value, tb)
+        f=open('error.txt', 'a')
+        f.write("\n".join(lines))
+        f.close()
+        print "\n".join(lines)
+sys.excepthook=Myexcepthook
 
 
 #INSTALLATION DU COOKIE
@@ -12,6 +21,7 @@ cj = cookielib.LWPCookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 opener.addheaders = [('User-agent', 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'), ('Connection' , 'Keep-Alive')]
 urllib2.install_opener(opener)
+
 
 def html(s):
     f=open("a.html","w")
@@ -31,17 +41,23 @@ def request(URL, data=None, headers={}, param=0):
         return data, handle, req
 
 def write_file(file, s):
-    f=open(file,"w")
+    f=open(file, 'wb')
     f.write(s)
     f.close()
 
-
+
+
+LIEN_IMAGES = "http://checkcode.taobao.com/auction/checkcode?sessionID=60b6ffe68495e74ad6dcb6fa1c91626a"
+
+
+
+
 def save_image(i):
     a, b, req1 = request(LIEN_IMAGES, param=1)
-    write_file("Images/Image%03d.jpg"%i, a)
+    write_file("Taobao/Image%03d.jpg"%i, a)
     print i
 
 
-for i in range(100,200):
+for i in range(1000):
     save_image(i)
 
