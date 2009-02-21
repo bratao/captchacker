4e886a305a4000ddcfbccd1a137a2e5841b64357
remi.paucher@student.ecp.fr <remi.paucher@student.ecp.fr@2e5afe5e-f900-11dd-b556-9305e745af9e>
2009-02-21 22:38:31 +0000

    Commit du fichier de la génération de la base de données simulée (oublié lors du dernier commit) !! Modèle SVM entrainé avec les données simulées => 99.8% de succès :-)) !! (Il faut relabllker des captchas pour faire un dossier de test)

---
+++ Egoshare/generate_simulated_db.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+
+LABELLED_CAPTCHAS_FOLDER = "Labelled Catpchas"
+DEST_FOLDER = "Simulated_digits"
+
+for i in range(10):
+    folder = os.path.join(DEST_FOLDER, str(i))
+    if not os.path.isdir(folder):
+        os.mkdir(folder)
+
+for folder, subfolders, files in os.walk(LABELLED_CAPTCHAS_FOLDER):
+    for file in [file for file in files if file[-4:] == ".jpg"]:
+        filename = os.path.join(LABELLED_CAPTCHAS_FOLDER, file)
+        print file
+        
+        if os.name == "nt":
+            str = '""'+os.path.join(os.getcwd(), 'Egoshare.exe" "'+filename+'""')
+            os.system(str)
+        else:
+            str = os.path.join("\ ".join(os.getcwd().split(" ")), "\ ".join('Egoshare Preprocessing'.split(' '))+" "+"\ ".join(filename.split(" ")))
+            os.system(str)
+
+        name1 = file[:-4]+"number_1.bmp"
+        name2 = file[:-4]+"number_2.bmp"
+        name3 = file[:-4]+"number_3.bmp"
+        
+        os.rename("letter1.bmp", name1)
+        os.rename("letter2.bmp", name2)
+        os.rename("letter3.bmp", name3)
+        
+        shutil.move(name1, os.path.join(DEST_FOLDER, file[0]))
+        shutil.move(name2, os.path.join(DEST_FOLDER, file[1]))
+        shutil.move(name3, os.path.join(DEST_FOLDER, file[2]))
+
+
+
--- Egoshare_3_TrainTestSVM.py
+++ Egoshare_3_TrainTestSVM.py
@@ -11,12 +11,12 @@ KERNEL_TYPE = [RBF]
 for C in CRANGE:
     for KERNEL in KERNEL_TYPE:
         
-        print C, KERNEL
         TRAINING_FOLDER = 'Egoshare/DBTraining'
+        TRAINING_FOLDER = 'Egoshare/Simulated_digits'
         TEST_FOLDER = 'Egoshare/DBTest'
         VERBOSE = 0
         MODEL_FOLDER = 'Egoshare/Models'
-        MODEL_FILE = "model_aligny=0.7_C="+str(C)+"_KERNEL="+str(KERNEL)+".svm"
+        MODEL_FILE = "model_WITH_CAPTCHA_DATA_C="+str(C)+"_KERNEL="+str(KERNEL)+".svm"
         GENERATE_ANYWAY = 1
 
         #Génération du modèle
@@ -24,8 +24,6 @@ for C in CRANGE:
         
         #Test du modèle
         execfile("Break_Egoshare_Captcha.py")
-        
-        time.sleep(1)
 
 
 raw_input()
