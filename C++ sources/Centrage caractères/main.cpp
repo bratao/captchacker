1440f9eb22b51513a648203799fe934522eb533e
hellwoxx <hellwoxx@2e5afe5e-f900-11dd-b556-9305e745af9e>
2009-02-24 18:56:55 +0000

    Version portable du prog C++ de centrage en utilisant boost::filesystem. Recursion dans le code C++ et non plus dans le code python.

--- C++ sources/Centrage caractères/main.cpp
+++ C++ sources/Centrage caractères/main.cpp
@@ -1,119 +1,113 @@
+#include <iostream>
+#include <string>
+#include "boost/filesystem.hpp"
+
 #include "cv.h"
 #include "cxcore.h"
 #include "highgui.h"
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <sstream>
 
-#include <iostream>
-#include "windows.h"
-#include "stdio.h"
-#include "dos.h" 
-#include <sstream>
-
-#include <cstdlib>
-#include <conio.h>
-
-
-
-#if defined (WIN32)
-#pragma comment(lib,"cv")
-#pragma comment(lib,"cvaux")
-#pragma comment(lib,"cxcore")
-#pragma comment(lib,"highgui")
-#pragma comment(lib,"cvcam")
-#endif
 
 using namespace std;
-
-
-
-string narrow( wstring& str )
-{
-	ostringstream stm ;
-	const ctype<char>& ctfacet =
-		use_facet< ctype<char> >( stm.getloc() ) ;
-	for( size_t i=0 ; i<str.size() ; ++i )
-		stm << ctfacet.narrow( str[i], 0 ) ;
-	return stm.str() ;
-}
-
-
+using namespace boost::filesystem;
 
 
 void process_file(string filenameIN, int WIDTH, int HEIGHT)
 {
 	//cout << "processing file: " << filenameIN << endl;
-	//Chargement de l'image
 	IplImage *srcImg=0, *res=0;
+
 	srcImg = cvLoadImage(filenameIN.c_str(),0);
 
-	res = cvCreateImage( cvSize(WIDTH, HEIGHT), IPL_DEPTH_8U, 1 );
-	cvFillImage(res, 255);
+    if (srcImg) {
+        res = cvCreateImage( cvSize(WIDTH, HEIGHT), IPL_DEPTH_8U, 1 );
+        cvFillImage(res, 255);
 
+        int xmin=WIDTH, xmax=0, ymin=HEIGHT, ymax=0;
+        for (int i=0; i<srcImg->width; ++i)
+        {
+            for (int j=0; j<srcImg->height; ++j)
+            {
+                if (cvGet2D(srcImg, j, i).val[0] == 0)
+                {
+                    if (i<xmin)
+                        xmin = i;
+                    if (i>xmax)
+                        xmax = i;
+                    if (j<ymin)
+                        ymin=j;
+                    if (j>ymax)
+                        ymax=j;
+                }
+            }
+        }
 
-	int xmin=WIDTH, xmax=0, ymin=HEIGHT, ymax=0;
-	for (int i=0; i<srcImg->width; ++i)
-	{
-		for (int j=0; j<srcImg->height; ++j)
-		{
-			if (cvGet2D(srcImg, j, i).val[0] == 0)
-			{
-				if (i<xmin)
-					xmin = i;
-				if (i>xmax)
-					xmax = i;
-				if (j<ymin)
-					ymin=j;
-				if (j>ymax)
-					ymax=j;
-			}
-		}
-	}
+        int offsetx = (WIDTH - (xmax-xmin))/2;
+        int offsety = (HEIGHT - (ymax-ymin))/2;
+        for (int i=0; i<=xmax-xmin; ++i)
+        {
+            for (int j=0; j<=ymax-ymin; ++j)
+            {
+                if ((offsety+j>0) && (offsety+j<res->height) && (offsetx+i>0) && (offsetx+i<res->width))
+                    cvSet2D(res, offsety+j, offsetx+i, cvGet2D(srcImg, ymin+j, xmin+i));
+            }
+        }
 
-	int offsetx = (WIDTH - (xmax-xmin))/2;
-	int offsety = (HEIGHT - (ymax-ymin))/2;
-	for (int i=0; i<=xmax-xmin; ++i)
-	{
-		for (int j=0; j<=ymax-ymin; ++j)
-		{
-			if ((offsety+j>0) && (offsety+j<res->height) && (offsetx+i>0) && (offsetx+i<res->width))
-				cvSet2D(res, offsety+j, offsetx+i, cvGet2D(srcImg, ymin+j, xmin+i));
-		}
-	}
-
-	cvSaveImage(filenameIN.c_str(), res);
+        cvSaveImage(filenameIN.c_str(), res);
+    }
+    else {
+        cout << "WARNING: File not found!!" << endl;
+    }
 
 }
 
 
+void process_folder(path folder, int WIDTH, int HEIGHT) {
+    cout << "Processing "<<folder.native_directory_string() << " folder... ";
+    if ( !exists(folder ) ) {
+        cout << "Folder not found. Aborting.";
+        exit(1);
+    }
 
+    directory_iterator itr(folder), end_itr;
 
+    for ( ; itr != end_itr; ++itr ){
+        if (is_directory(itr->status())){
+            process_folder(itr->path(), WIDTH, HEIGHT);
+        }
+        else if (is_regular(itr->status())) {
+            string filename = itr->leaf();
+            if (filename.substr(filename.length()-3,3) == "bmp") {
+                //cout<<itr->leaf()<< " => bmp found!!"<< endl;
+                process_file(folder.native_directory_string()+"/"+itr->leaf(), WIDTH, HEIGHT);
+            }
+        }
+
+    }
+    cout << "Done." << endl;
+}
 
 
 
 int main(int argc, char *argv[])
 {
-	//Noms de fichiers
-	string folder;
-
-	//cout << "ARGC: " << argc << endl;
+	path folder;
 
 	int WIDTH;
 	int HEIGHT;
 
 	if (argc < 2)
 	{
-		folder = "*";
-		WIDTH = 38;
-		HEIGHT = 31;
+		/*folder = "./DBTraining-Simulation_based";
+		WIDTH = 20;
+		HEIGHT = 20;*/
+
+		cout << "Usage:"<<endl<<"   Linux:     Centrage path/to/folder WIDTH HEIGHT"<<endl<<"   Windows:   Centrage.exe path/to/folder WIDTH HEIGHT"<<endl;
+		exit(1);
 	}
 	else
 	{
 		folder = argv[1];
-		//cout << "filename IN " << folder << endl;
+		//cout << "folder IN " << folder << endl;
 
 		WIDTH = atoi(argv[2]);
 		//cout << "WIDTH " << WIDTH << endl;
@@ -122,66 +116,9 @@ int main(int argc, char *argv[])
 		//cout << "HEIGHT " << HEIGHT << endl;
 	}
 
-
-	string expression_bmp = folder + "\\*.bmp";
-	std::vector<std::wstring> SongsLoaded;
-
-	//InitFunction
-	WIN32_FIND_DATA findFileData;
-
-
-
-	char *expr = (char *) expression_bmp.c_str();
-	wchar_t *lpfile = new wchar_t[200];
-	mbstowcs(lpfile, expr, strlen(expr));
-
-	//cout << "sizeof: " << sizeof(expr) << endl;
-	//cout << "EXPRESSION_BMP CHAR*: " << expr << endl;
-	//wcout << "EXPRESSION_BMP: " << lpfile << endl;
-
-	HANDLE hFind = FindFirstFile(lpfile, &findFileData);
-
-	if(hFind  == INVALID_HANDLE_VALUE)
-	{
-		//cout << "Could Not Find Any file in folder!" << endl;
-		//system("pause");
-		return 1;
-	}
-
-	SongsLoaded.push_back(findFileData.cFileName);
-
-	while(FindNextFile(hFind, &findFileData))
-	{
-		SongsLoaded.push_back(findFileData.cFileName);
-		//wcout << findFileData.cFileName << endl;
-	}
-
-	FindClose(hFind);
-
-	for (int i=0; i<SongsLoaded.size(); ++i)
-	{
-		//wcout << "Traitement: " << SongsLoaded[i] << endl;
-
-		string filename = narrow(SongsLoaded[i]);
-		filename = folder + "\\" + filename;
-
-		process_file(filename, WIDTH, HEIGHT);
-
-		//getchar();
-
-	}
-	//cout << endl;
-
-
-
-
-
-
-	//system("pause");
+	process_folder(folder, WIDTH, HEIGHT);
 
 	return 0;
-
-
 }
 
 
--- characters_center.py
+++ characters_center.py
@@ -6,17 +6,14 @@ psyco.full()
 WIDTH = 20
 HEIGHT = 20
 
-TRAINING_FOLDER = "Egoshare/DBTraining-Simulation_based"
+folder_to_process = "Egoshare/DBTraining-Simulation_based"
 
-for folder, subfolders, files in os.walk(TRAINING_FOLDER):
-    
-    print folder
-        
-    if os.name == "nt":
-        command = 'centrage.exe "%s" %d %d'%(folder, WIDTH, HEIGHT)
-    else:
-        #Some Linux stuff :)
-        pass
-    
-    os.system(command)
+if os.name == "nt":
+    command = 'centrage.exe "%s" %d %d'%(folder_to_process, WIDTH, HEIGHT)
+elif os.name == "posix":
+    command = './Centrage %s %d %d'%(folder_to_process, WIDTH, HEIGHT)
+else:
+    print "OS non supported"
+    sys.exit(0)
 
+os.system(command)
